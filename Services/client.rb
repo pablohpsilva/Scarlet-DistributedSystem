69875506352c7392f6210718557e5e56bb67679e
RicardoBC <ricardomamuteb@gmail.com>
2014-11-21 20:51:02 -0300

    Cliente sem HTTP

--- Services/client.rb
+++ Services/client.rb
@@ -24,27 +24,22 @@ if ARGV.length >= 3
 	print "\n\n"
 	print headers
 	
-	if ARGV.length > 3
-		puts "\n\nmethod: #{$method}\n\n"
-		
-		uri = URI('localhost')
-		puts "\n\nURI = #{uri}\n"
-		if $data != nil
-			t = "http://"+$host+$method +"?acao="+$action+"&value="+$value+"&dado="+$data
-		else
-			t = "http://"+$host+$method +"?acao="+$action+"&value="+$value
-		end
-		http = Net::HTTP.new($host, $port) # Abre um conexão HTTP com o servidor
-		#req = Net::HTTP::Post.new($method) # Faz uma requisição para um arquivo		
-		req = Net::HTTP::Post.new(t) # Faz uma requisição para um arquivo		
-		req.set_form_data("acao" => $action, "nome" => $value)		
-		response = http.request(req)		
-		
-		puts "req = #{req}"
-		puts "response = #{response}"
-		puts "response.body = #{response.body}"		
-		
+elsif ARGV.length > 3
+	socket = TCPSocket.open($host,$port)  	# Connect to server
+	if $data != nil
+		request = "http://"+$host+":8888"+$method +"?acao="+$action+"&value="+$value+"&dado="+$data
+	else
+		request = "http://"+$host+":8888"+$method +"?acao="+$action+"&value="+$value
 	end
+
+	socket.print("POST #{request} HTTP/1.0\r\n\r\n")
+
+	response = socket.read              	# Read complete response
+	headers, body = response.split("\r\n\r\n", 2)
+	print body                 	# And display it
+	print "\n\n"
+	print headers		
+		
 else
   print "Nope. Try again \n"
 end
